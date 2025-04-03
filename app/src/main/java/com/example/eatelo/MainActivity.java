21e06e127d7cacd50a5c2716cf6750da98c753a4
Vansh Yadav <66347494+norsechurros@users.noreply.github.com>
2025-04-03 12:26:14 +0530

    preferences page + functionality and ranking page + func done, beautification needed

--- app/src/main/java/com/example/eatelo/MainActivity.java
+++ app/src/main/java/com/example/eatelo/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.eatelo;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Button to navigate to Ranking Page
+        findViewById(R.id.button13).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to RankingPageActivity
+                Intent intent = new Intent(MainActivity.this, PreferencePageActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
